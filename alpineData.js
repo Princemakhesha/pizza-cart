e07055f93e2b6c034a02ad1006a4501b8ab406c7
Prince Makhesha <princemakhesha@gmail.com>
2023-06-28 09:01:07 +0200

    second commit

---
+++ alpineData.js
@@ -0,0 +1,27 @@
+// document.addEventListener('alpine:init', () => {
+//     Alpine.data('cart', () => ({
+//         // Your code goes here
+//         qty: 1,
+//         message:'',
+//         enteredMessage:'',
+//         cartObj:[
+//             {
+//                 name:'',
+//                 price:200,
+//                 qty:3,
+
+//             }
+//         ],
+
+//         addToCart() {
+//             console.log("adding to cart");
+//             // your logic goes here
+
+            
+//             this.cartObj.push({
+//                 name:''
+//             })
+//         }
+//     }))
+// })
+
